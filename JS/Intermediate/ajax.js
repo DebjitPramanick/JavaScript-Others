0cabbb0440561789491fbbd701e743d7a5972800
DebjitPramanick <dpramanick9990@gmail.com>
2021-02-06 18:12:23 +0530

    Version 1

--- JS/Intermediate/ajax.js
+++ JS/Intermediate/ajax.js
@@ -6,8 +6,16 @@ console.log("Let's learn AJAX.")
 
 
 let fetchBtn = document.getElementById('fetch-btn');
+const backupBtn = document.getElementById('backup-btn');
 
-fetchBtn.addEventListener('click', () => {
+fetchBtn.addEventListener('click', getData) 
+backupBtn.addEventListener('click', postData) 
+
+// 	http://dummy.restapiexample.com/api/v1/create
+
+
+
+function getData(){
 
     console.log("Button Clicked!")
 
@@ -15,23 +23,58 @@ fetchBtn.addEventListener('click', () => {
     const xhr = new XMLHttpRequest();
 
     // Opening the object
-    xhr.open('GET', 'https://randomuser.me/api/', true);
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/', true);
 
     // What to do on progress
-    xhr.onprogress = () =>{
+    xhr.onprogress = () => {
         console.log("On progress ....")
     }
-    
-    
-    // Send the request
-    xhr.send();
 
 
     // What to do onload
-    xhr.onload = () =>{
-        console.log(this.response);
+    xhr.onload = () => {
+        const obj = JSON.parse(xhr.responseText);
+        console.log(obj)
+
+        let list = document.getElementsByClassName("list")[0];
+        str = " ";
+
+        for(key in obj){
+            str += `<p>${obj[key].title}</p>`
+        }
+        list.innerHTML = str
     }
 
 
-    
-})
+    // Send the request
+    xhr.send();
+}
+
+
+
+function postData(){
+    console.log("Button Clicked!")
+
+    // Creating an xhr object
+    const xhr = new XMLHttpRequest();
+
+    // Opening the object
+    xhr.open('POST', 'http://dummy.restapiexample.com/api/v1/create', true);
+    xhr.getAllResponseHeaders('Content-type','application/json')
+
+    // What to do on progress
+    xhr.onprogress = () => {
+        console.log("On progress ....")
+    }
+
+
+    // What to do onload
+    xhr.onload = () => {
+        console.log(xhr.responseText);
+    }
+
+
+    // Send the request
+    params = { "name": "test", "salary": "123", "age": "23" }
+    xhr.send(params);
+}
