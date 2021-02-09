4bcd71ca80b3a157faf9c3d466a3ab9f511b1b05
DebjitPramanick <dpramanick9990@gmail.com>
2021-02-09 12:05:42 +0530

    Version 1

---
+++ JS/Intermediate/fetchAPI.js
@@ -0,0 +1,30 @@
+console.log("Fetch API...")
+
+const myBtn = document.getElementById("myBtn");
+const myBtn2 = document.getElementById("myBtn2");
+const myContent = document.getElementById("myContent");
+
+myBtn.addEventListener('click',()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
+        console.log(data)
+    })
+})
+
+
+
+myBtn2.addEventListener('click', () => {
+    let url = 'http://dummy.restapiexample.com/api/v1/create';
+    let data = '{ "name": "debjit", "salary": "12003", "age": "23" }';
+    params = {
+        method: 'post',
+        
+        body: data
+    }
+    fetch(url,params)
+    .then(res => res.json())
+    .then(data => console.log(data))
+})
