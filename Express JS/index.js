b6791387fb6a9b48d16e671eec440b86fd0be13b
DebjitPramanick <dpramanick9990@gmail.com>
2021-02-17 13:04:36 +0530

    Version 1

--- Express JS/index.js
+++ Express JS/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const cors = require('cors')
 const framworkRoute = require('./Routers/framework.js')
 
@@ -8,6 +9,16 @@ app.use(cors())
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server created at ${port}.....`))
+const filePath = path.join(__dirname, "./views")
+
+// To set the view engine
+
+app.set("view engine","hbs")
+app.get('/', (req,res)=>{
+    res.render('index.hbs')
+})
+
+app.use(express.static(filePath))
 app.get('/',(req,res)=> {
     res.send("Server connected....")
 })
